f9a7a69598b7c620f9e54c82549a469f9b3d2952
genciyusifov2 <asgaraliyev.work@gmail.com>
2024-10-30 14:24:30 +0400

    ads

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,5 +1,6 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 // 
+// 
  const config:DataSourceOptions={
     type: 'postgres', // Veritabanı türü
    host: 'dpg-csb83ue8ii6s7383sjpg-a.oregon-postgres.render.com', // Veritabanı host'u
