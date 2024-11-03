1f50e7ad354169c5342d84686431f645fd9705d2
genciyusifov2 <asgaraliyev.work@gmail.com>
2024-11-03 19:20:19 +0400

    Update product.entity.ts

--- src/products/product.entity.ts
+++ src/products/product.entity.ts
@@ -1,34 +1,30 @@
 import { IsString } from 'class-validator';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
-// kateqoriya: category,
-// mehsulun_adi: productName,
-// mayaDeyeri: costPrice,
-// satisQiymeti: salePrice,
-import  {v4 as uuidv4} from "uuid"
+
 @Entity("product")
 export class Product {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({type:"varchar"})
-  mehsulun_adi:string;
+  @Column({ type: "varchar" })
+  mehsulun_adi: string;
 
-  @Column({type:"varchar"})
-  kateqoriya:string;
+  @Column({ type: "varchar" })
+  kateqoriya: string;
 
-  @Column({type:"varchar"})
-  mayaDeyeri:string;
+  @Column({ type: "varchar" })
+  mayaDeyeri: string;
 
-  @Column({type:"varchar"})
-  satisQiymeti:string;
+  @Column({ type: "varchar" })
+  satisQiymeti: string;
 
-  constructor(product:Product){
-    if(product){
-      this.id=this.generateId()
-      Object.assign(this,product)
+  // Yeni stocSayisi sütunu
+  @Column({ type: "int", default: 0 })
+  stocSayisi: number;
+
+  constructor(product: Partial<Product>) {
+    if (product) {
+      Object.assign(this, product);
     }
-  }
-  generateId(){
-    return uuidv4()
   }
 }
