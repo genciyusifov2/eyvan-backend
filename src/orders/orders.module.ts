a8abd9b1312acaea696ed0d70f83509b4774ad59
genciyusifov2 <asgaraliyev.work@gmail.com>
2024-11-03 22:11:44 +0400

    Update orders.module.ts

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { Order } from './order.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Order])],
     controllers:[OrdersController],
     providers:[OrdersService]
 })
