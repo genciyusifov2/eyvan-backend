0155d38bf16b59ab7ff3f6a3e7d3a356d33d5acd
genciyusifov2 <asgaraliyev.work@gmail.com>
2024-11-23 01:05:00 +0400

    Update typeorm.config.ts

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,7 +2,7 @@ import { DataSource } from "typeorm";
 
 export const dataSource = new DataSource({
   type: "postgres",
-  host: "145.223.83.25", // Eğer VPS'ten bağlanıyorsanız localhost kullanın.
+  host: "localhost", // Eğer VPS'ten bağlanıyorsanız localhost kullanın.
   port: 5432, // Varsayılan PostgreSQL portu
   username: "admin", // PostgreSQL kullanıcı adı
   password: "Qwerty4876__", // PostgreSQL şifresi
