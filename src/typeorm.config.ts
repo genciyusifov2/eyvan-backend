dc70b91d2886705338b0103c786518a4ae739a71
genciyusifov2 <asgaraliyev.work@gmail.com>
2024-11-23 00:07:14 +0400

    Update typeorm.config.ts
    update2

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -2,22 +2,19 @@ import { DataSource, DataSourceOptions } from "typeorm";
 
 const config: DataSourceOptions = {
   type: 'postgres',
-  host: 'dpg-csb83ue8ii6s7383sjpg-a.oregon-postgres.render.com',
-  port: 5432,
-  username: 'eyvan',
-  password: 'xdkWg6tpWyjHbZM7lI09jQVVhZNju3tn',
-  database: 'eyvan',
-  migrationsTableName: "migration", // Düzgün yazım
+  host: 'localhost', // VPS üzerinde localhost
+  port: 5432, // PostgreSQL'in varsayılan portu
+  username: 'admin', // PostgreSQL kullanıcısı
+  password: 'Qwerty4876__', // PostgreSQL şifresi
+  database: 'eyvandb', // Kullanacağınız veritabanı
   migrationsRun: true,
-  entities: [__dirname + '/**/*.entity{.ts,.js}'], // Entity yolunun doğru olması için
-  migrations: [__dirname + '/**/*.migration{.ts,.js}'], // Migration yolunun doğru olması için
-  synchronize: false, // Üretim için false yapın
-  ssl: {
-    rejectUnauthorized: false // SSL parametresi gerekebilir
-  }
+  entities: [__dirname + '/**/*.entity{.ts,.js}'], // Entity dosyalarının yolu
+  synchronize: true, // Otomatik senkronizasyon
+  ssl: false // SSL bağlantısı gerekiyorsa true yapabilirsiniz
 };
 
 export const dataSource = new DataSource(config);
+
 
 // Veritabanı bağlantısını test etme
 dataSource
